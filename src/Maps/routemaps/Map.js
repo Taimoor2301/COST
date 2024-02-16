96652bf393dc5482a1a9ed42b5864fe499836437
Taimoor Ali <taimoorali4214@gmail.com>
2024-02-16 15:13:47 +0500

    fix use effect error

--- src/Maps/routemaps/Map.js
+++ src/Maps/routemaps/Map.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef , useEffect } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
